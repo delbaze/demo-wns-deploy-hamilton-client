2fc15d00eb30d08b9d6f9d9082cf846038b4f56e
David <d.elbaze@gmail.com>
2023-03-29 16:16:50 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ function App() {
     <div>
       <Header>
         <Container>
-          <h1>Wilders Book with CD</h1>
+          <h1>Wilders Book with CD avec modif</h1>
         </Container>
       </Header>
       <Container>
